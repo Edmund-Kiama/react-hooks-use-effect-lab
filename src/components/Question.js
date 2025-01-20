de6b2f1cada91e123cadea3aa9094aff5b5ac272
Edmund <edmundgachanja@gmail.com>
2025-01-20 10:15:43 +0300

    Completes the lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,29 @@
+import { useEffect } from "react";
 import React, { useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    const timerId = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered])
+
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
+    setTimeRemaining();
     onAnswered(isCorrect);
   }
 
